98a3ca7341ef23219f2adb868ea8055c014ec664
Neice <52137719+TrenyceCodes@users.noreply.github.com>
2024-09-28 02:16:39 -0400

    Finish setting up Rock Paper Scissors game ui and game mechanics

---
+++ RockPaperScissorsGame.py
@@ -0,0 +1,103 @@
+import random
+from tkinter import *
+from tkinter import messagebox
+from PIL import ImageTk, Image
+
+GameRoot = Tk()
+
+class RockPaperScissorsGame:
+    user_points: int
+    ai_points: int
+    game_match: int
+    ai_options: list[str]
+
+    def __init__(self, game_root: Tk):
+        self.game_root = game_root
+        self.game_root.title("Rock Paper Scissors Game")
+        self.game_root.resizable(False, False)
+
+        self.user_points = 0
+        self.ai_points = 0
+        self.game_match = 3
+        self.ai_options = ["rock", "paper", "scissors"]
+
+        self.handle_managing_widgets()
+
+    def handle_managing_widgets(self):
+        self.user_score_label = Label(GameRoot, text=f"Your Score: {self.user_points}")
+        self.user_score_label.grid(column=0, row=0)
+
+        self.ai_score_label = Label(GameRoot, text=f"AI Score: {self.ai_points}")
+        self.ai_score_label.grid(column=3, row=0)
+
+        self.current_score_label = Label(GameRoot, text=f"Current Score - User Score: {self.user_points} - AI score: {self.ai_points}")
+        self.current_score_label.grid(column=1, row=2)
+
+        self.ai_decision_label = Label(GameRoot, text=f"AI chose: ")
+        self.ai_decision_label.grid(column=3, row=2)
+
+        self.user_decision_label = Label(GameRoot, text=f"You chose: ")
+        self.user_decision_label.grid(column=0, row=2)
+
+        self.result_label = Label(GameRoot, text=f"")
+        self.result_label.grid(column=1, row=4)
+
+        Button(GameRoot, text="Rock", command=lambda: self.handle_winner("rock"), width=20, height=20).grid(
+            column=0,
+            row=1)
+        Button(GameRoot, text="Scissors", command=lambda: self.handle_winner("scissors"), width=20,
+               height=20).grid(
+            column=1,
+            row=1)
+        Button(GameRoot, text="Paper", command=lambda: self.handle_winner("paper"), width=20, height=20).grid(
+            column=3,
+            row=1)
+        Button(GameRoot, text="Exit Game", command=self.game_root.destroy).grid(column=1, row=0)
+
+    def handle_winner(self, user_choice: str):
+        ai_decision = random.choice(self.ai_options)
+
+        print(f"Users decision: {user_choice}")
+        print(f"Ai decision: {ai_decision}")
+        self.user_decision_label.config(text=f"Users decision: {user_choice}")
+        self.ai_decision_label.config(text=f"Ai decision: {ai_decision}")
+
+        if user_choice == ai_decision:
+            print(f"It's a draw! Pick another choice")
+            self.result_label.config(text=f"It's a draw! Pick another choice")
+        elif (user_choice == "rock" and ai_decision == "scissors") or \
+                (user_choice == "paper" and ai_decision == "rock") or \
+                (user_choice == "scissors" and ai_decision == "paper"):
+            print(f"{user_choice} beats {ai_decision}")
+            self.result_label.config(text=f"{user_choice} beats {ai_decision}")
+            self.user_points += 1
+            self.user_score_label.config(text=f"User Score: {self.user_points}")
+        else:
+            print(f"{ai_decision} beats {user_choice}")
+            self.ai_points += 1
+            self.ai_score_label.config(text=f"AI Score: {self.ai_points}")
+            self.result_label.config(text=f"{ai_decision} beats {user_choice}")
+
+        print(f"Current Score - User Score: {self.user_points} - AI score: {self.ai_points}")
+        self.current_score_label.config(text=f"Current Score - User Score: {self.user_points} - AI score: {self.ai_points}")
+
+        if self.user_points == self.game_match:
+            print(f"User has won the match: {self.user_points}")
+            messagebox.showinfo("Result", f"User has won the match: {self.user_points}")
+            self.reset_game()
+
+        if self.ai_points == self.game_match:
+            print(f"Ai has won the match: {self.ai_points}")
+            messagebox.showinfo("Result", f"AI has won the match: {self.ai_points}")
+            self.reset_game()
+
+    # resets the game
+    def reset_game(self):
+        self.ai_points = 0
+        self.user_points = 0
+        self.user_score_label.config(text=f"Your Score: 0")
+        self.ai_score_label.config(text=f"AI Score: 0")
+
+
+game = RockPaperScissorsGame(GameRoot)
+GameRoot.mainloop()
--- main.py
+++ main.py
@@ -27,32 +27,33 @@ ai_options = ["rock", "paper", "scissors"]
 #handles who is the winner of the game
 def handle_winner(user_choice: str):
     global user_points, ai_points
-    while user_points < game_match and ai_points < game_match:
-        ai_decision = random.choice(ai_options)
+    ai_decision = random.choice(ai_options)
 
-        print(f"Users decision: {user_choice}")
-        print(f"Ai decision: {ai_decision}")
+    print(f"Users decision: {user_choice}")
+    print(f"Ai decision: {ai_decision}")
 
-        #runs if loop to check for multiple decisions
-        if user_choice == ai_decision:
-            print(f"It's a draw! Pick another choice")
-        elif (user_choice == "rock" and ai_decision == "scissors") or \
-                (user_choice == "paper" and ai_decision == "rock") or \
-                (user_choice == "scissors" and ai_decision == "paper"):
-            print(f"{user_choice} beats {ai_decision}")
+    #runs if loop to check for multiple decisions
+    if user_choice == ai_decision:
+        print(f"It's a draw! Pick another choice")
+    elif (user_choice == "rock" and ai_decision == "scissors") or \
+            (user_choice == "paper" and ai_decision == "rock") or \
+            (user_choice == "scissors" and ai_decision == "paper"):
+        print(f"{user_choice} beats {ai_decision}")
 
-            user_points += 1
-        else:
-            print(f"{ai_decision} beats {user_choice}")
-            ai_points += 1
+        user_points += 1
+    else:
+        print(f"{ai_decision} beats {user_choice}")
+        ai_points += 1
 
-        print(f"Current Score - User Score: {user_points} - AI score: {ai_points}")
+    print(f"Current Score - User Score: {user_points} - AI score: {ai_points}")
+
     if user_points == game_match:
         print(f"User has won the match: {user_points}")
         reset_game()
     else:
         print(f"Ai has won the match: {ai_points}")
         reset_game()
+
 
 #resets the game
 def reset_game():
@@ -79,10 +80,13 @@ GameForm.grid()
 
 Label(GameRoot, text=f"Your Score: {user_points}").grid(column=0, row=0)
 Label(GameRoot, text=f"AI Score: {ai_points}").grid(column=3, row=0)
-Button(GameRoot, image=rock_photo, command=handle_winner("rock"), width=220, height=220, pady=20).grid(column=0, row=1)
-Button(GameRoot, image=scissors_photo, command=handle_winner("scissors"), width=220, height=220, pady=20).grid(column=1,
+Button(GameRoot, image=rock_photo, command=lambda: handle_winner("rock"), width=220, height=220, pady=20).grid(column=0,
                                                                                                                row=1)
-Button(GameRoot, image=paper_photo, command=handle_winner("paper"), width=220, height=220, pady=20).grid(column=3,
-                                                                                                         row=1)
+Button(GameRoot, image=scissors_photo, command=lambda: handle_winner("scissors"), width=220, height=220, pady=20).grid(
+    column=1,
+    row=1)
+Button(GameRoot, image=paper_photo, command=lambda: handle_winner("paper"), width=220, height=220, pady=20).grid(
+    column=3,
+    row=1)
 Button(GameRoot, text="Exit Game", command=GameRoot.destroy).grid(column=1, row=0)
 GameRoot.mainloop()
